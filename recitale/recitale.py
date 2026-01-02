318258ff226bade131e424d068929a10d8caa0a1
Quentin Schulz <foss+recitale@0leil.net>
2026-01-02 19:03:53 +0100

    recitale: factor out themes directory into a variable
    The path to the parent directory of all themes is repeated many times
    and is not very readable. Let's make it a variable so it's both easier
    to read and maintain. Added benefit is that this makes lines shorter as
    well.
    
    Signed-off-by: Quentin Schulz <foss+recitale@0leil.net>

--- recitale/recitale.py
+++ recitale/recitale.py
@@ -156,10 +156,11 @@ def get_local_date_filter(date_locale):
 def get_gallery_templates(
     theme, gallery_path="", parent_templates=None, date_locale=None
 ):
-    theme_path = Path(__file__).parent.joinpath("themes", theme).exists()
+    themes_dir = Path(__file__).parent.joinpath("themes")
+    theme_path = themes_dir.joinpath(theme).exists()
 
     available_themes = theme, "', '".join(
-        str(path) for path in Path(__file__).parent.joinpath("themes").iterdir()
+        str(path) for path in themes_dir.iterdir()
     )
 
     if not theme_path:
@@ -172,12 +173,12 @@ def get_gallery_templates(
 
     templates_dir = [
         Path(".").joinpath("templates").resolve(),
-        Path(__file__).parent.joinpath("themes", theme, "templates"),
+        themes_dir.joinpath(theme, "templates"),
     ]
 
     if theme != "exposure":
         templates_dir.append(
-            Path(__file__).parent.joinpath("themes", "exposure", "templates")
+            themes_dir.joinpath("exposure", "templates")
         )
 
     subgallery_templates = Environment(
@@ -202,7 +203,7 @@ def get_gallery_templates(
 
     else:
         shutil.copytree(
-            Path(__file__).parent.joinpath("themes", theme, "static"),
+            themes_dir.joinpath(theme, "static"),
             Path(".").joinpath("build", gallery_path, "static"),
         )
 
